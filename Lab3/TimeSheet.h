47da37fb9e4d97ba1f5bf24fa5281bd685b9e239
Jacob <tldk47@hanmail.net>
2022-05-16 16:44:21 +0900

    final version

--- Lab3/TimeSheet.h
+++ Lab3/TimeSheet.h
@@ -6,21 +6,21 @@ namespace lab3
 	class TimeSheet
 	{
 	public:
-		TimeSheet(const char* name, unsigned int maxEntries);
-		TimeSheet(const TimeSheet& copy);
-		TimeSheet& operator=(const TimeSheet& rhs);
-		~TimeSheet();
-		void AddTime(int timeInHours);
-		int GetTimeEntry(unsigned int index) const;
-		int GetTotalTime() const;
-		float GetAverageTime() const;
-		float GetStandardDeviation() const;
-		const std::string& GetName() const;
+		TimeSheet (const char* name, unsigned int maxEntries);
+		TimeSheet (const TimeSheet& copy);
+		TimeSheet& operator= (const TimeSheet& rhs);
+		~TimeSheet ();
+		void               AddTime (int timeInHours);
+		int                GetTimeEntry (unsigned int index) const;
+		int                GetTotalTime () const;
+		float              GetAverageTime () const;
+		float              GetStandardDeviation () const;
+		const std::string& GetName () const;
 
 	private:
 		// 필요에 따라 private 변수와 메서드를 추가하세요.
-		std::string mName;
-		int* mTimeStamp;
+		std::string  mName;
+		int*         mTimeStamp;
 		unsigned int mMaxEntry;
 		unsigned int mCurrentDay;
 	};
--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,8 +1,29 @@
+#include <cassert>
+
 #include "TimeSheet.h"
 using namespace lab3;
 
 int main()
 {
-	TimeSheet test("", 10);
+	TimeSheet jacob("Jacob Lim", 90);
+
+	/// add time to timeStamp
+	/// discard time if < 1 or >10
+	jacob.AddTime(3);
+	jacob.AddTime(10);
+	jacob.AddTime(11);
+
+	/// return all time stamps 
+	assert(jacob.GetTotalTime() == 13);
+
+	/// return time stamp's average time
+	assert(jacob.GetAverageTime() == 6.5f);
+
+	/// return owner's name
+	assert(jacob.GetName() == "Jacob Lim");
+
+	assert(jacob.GetTimeEntry(1) == 10);
+	
+	
 }
 
