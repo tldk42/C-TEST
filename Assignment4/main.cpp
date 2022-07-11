0a085d1af0dc86b9b0ca4619adc1ab571bf7b83d
Jacob <tldk47@hanmail.net>
2022-07-11 10:54:50 +0900

    1st TreeNode.h

--- Assignment4/main.cpp
+++ Assignment4/main.cpp
@@ -1,4 +1,11 @@
+#include "TreeNode.h"
+
+using namespace assignment4;
+
 int main()
 {
+	TreeNode<int> n(std::make_unique<int>(2));
 
+	std::shared_ptr<TreeNode<int>> parent = std::make_shared<TreeNode<int>>(std::make_unique<int>(5));
+	TreeNode<int> n2(parent, std::make_unique<int>(2));
 }
